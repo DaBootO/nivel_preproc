a1402e65704ca566b3ba8630c34e218183b5895b
DaBootO <dariocontrino@yahoo.de>
2022-05-18 00:04:25 +0200

    added spinner and finished data preprocessing

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import argparse
 from pandas import DataFrame
 from dbfread import DBF
 from itertools import islice
+from tqdm import tqdm
+from yaspin import yaspin
 
 def vprint(msg):
     """prints only when verbosity is switched on
@@ -53,6 +55,16 @@ def load_data(filename):
     else:
         print("Your supplied file is not an excel (.xlsx, .xlsm, .xltx, .xltm) or a .DBF file! We are currently not able to parse any other files")
         sys.exit(1)
+
+def convert_date(date):
+    try:
+        return date.to_pydatetime()
+    except AttributeError:
+        if isinstance(date, datetime.date):
+            return date
+        else:
+            print("date: %s could not be converted to a datetime.date object" % date)
+            sys.exit(1)
 
 formatter = lambda prog: argparse.ArgumentDefaultsHelpFormatter(prog,max_help_position=50, width=120)
 my_parser = argparse.ArgumentParser(
@@ -118,7 +130,44 @@ if args.file == None:
 
 #!# GETTING VARIABLES FROM ARGPARSE #!#
 fn = args.file
+#!# GETTING VARIABLES FROM ARGPARSE #!#
 
-vprint("loading file...")
+# sorry if all this spinning bullshit is too much, never used it and saw an opportunity here
+with yaspin(text="loading file...") as sp:
+    data = load_data(filename=fn)
+    sp.write("> finished loading file")
 
-data = load_data(filename=fn)
+# convert all dates to the needed format -> D_YYYYmmdd
+dates_with_repetitions = [convert_date(d).strftime('D_%Y%m%d') for d in data['Datum']]
+
+# switch out the Datum column with the new format -> easier to compare later
+data['Datum'] = dates_with_repetitions
+
+# list of all dates on which a nivellement measurement was done -> no repetitions
+singular_date_list = list(dict.fromkeys(dates_with_repetitions))
+
+# we will define an empty dict to put all the needed data into
+# the dict will be of following structure:
+# {NAME: X_koord, Y_koord, dict of dates the nivellement measurement was undertaken with their Z_koord} e.g.
+# dict = {
+    # PUNKTNAME_1: [X_1, Y_1, {DATE_1: Z_at_d1, DATE_3: Z_at_d3, DATE_9: Z_at_d9}],     
+    # PUNKTNAME_2: [X_2, Y_2, {DATE_2: Z_at_d2, DATE_3: Z_at_d3, DATE_5: Z_at_d5}],
+    # .     
+    # .     
+    # .     
+    # PUNKTNAME_N: [X_N, Y_N, {DATE_1: Z_at_d1, DATE_2: Z_at_d2, DATE_3: Z_at_d3}]
+    #}
+outputs = {}
+
+with tqdm(total=len(data)) as pbar:
+    for index, row in data.iterrows():
+        NR = row['Punktname_']
+        X_koord = row['X']
+        Y_koord = row['Y']
+        Z_koord = row['Z']
+        date = row['Datum']
+        if NR not in outputs.keys():
+            outputs[NR] = [X_koord, Y_koord, {date: Z_koord}]
+        else:
+            outputs[NR][2][date] = Z_koord
+        pbar.update(1)
